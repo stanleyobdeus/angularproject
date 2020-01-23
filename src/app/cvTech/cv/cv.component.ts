e3caa76921f0113061664a6577bedc50fbebee4a
OBDEUS Stanley <obdeus9stanley@gmail.com>
2020-01-23 12:29:48 -0500

    commit

--- src/app/cvTech/cv/cv.component.ts
+++ src/app/cvTech/cv/cv.component.ts
@@ -14,7 +14,6 @@ personnes: Personne[];
     this.personnes=[
       new Personne(1,'Stanley','OBDEUS',49,'rotating_card_profile2.png',98777,'Analyste Developpeur'),
       new Personne(2,'Vladimir','OBDEUS',149,'tome.jpg',987,'Lead Developpeur')
-      //stanley
     ]
   }
 
