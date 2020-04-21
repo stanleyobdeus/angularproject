a6f8c487f90dbb4a058c56cdcb69c510df2057a8
Stanley Obdeus <obdeus9stanley@gmail.com>
2020-04-21 13:04:10 -0400

    add paramettre when you make a request using HttpInterceptor

--- src/app/interceptors/login.interceptors.ts
+++ src/app/interceptors/login.interceptors.ts
@@ -6,13 +6,23 @@ import { Observable } from 'rxjs';
 
 export class LoginInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+       console.log(req.params.keys()); // it will display filter
        const token = localStorage.getItem('token');
        if (token) {
-        const cloneReq = req.clone(
-            {
-                params: new HttpParams().set('access_token', token)
-            }
-        );
+        const keys = req.params.keys();
+        let params = new HttpParams();
+         // tslint:disable-next-line: prefer-for-of
+         for (let index = 0; index < keys.length; index++){
+             params = params.append(keys[index], req.params.get(keys[index]));
+         }
+         params = params.append('access_token', token);
+       // const cloneReq = req.clone(
+          //  {
+            //    params: new HttpParams().set('access_token', token)
+          //  }
+       // );
+        const cloneReq = req.clone({params});
+
         return next.handle(cloneReq);
        } else {
         return next.handle(req);
